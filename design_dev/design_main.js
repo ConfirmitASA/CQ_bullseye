cea36a92a2c2f21bde5d181329279a78d0b49578
Maria Senina <maria.senina@confirmit.com>
2020-10-05 17:04:06 +0300

    init function for design

--- design_dev/design_main.js
+++ design_dev/design_main.js
@@ -1,45 +1,55 @@
 import Colors from "./bullseyeColors.js";
 import Images from "./images.js";
 
-let sizeInput = document.getElementById('size');
-let iconsSizeInput = document.getElementById('iconsSize');
-let centerTextInput = document.getElementById('centerText');
-let requiredInput = document.getElementById('numberRequiredAnswers');
-let centerIsActive = document.getElementById('centerIsActive');
-let centerTextColorInput = document.getElementById('centerTextColor');
-let itemsColorInput = document.getElementById('itemsColor');
-let itemsLayoutInput = document.getElementById('itemsLayout');
-let colorNumber = document.getElementById("colorsNumber");
-let imagesNumber = document.getElementById('imagesNumber');
-const colors = new Colors(saveChanges);
-const images = new Images(saveChanges);
+let language;
+let centerTextSetting = {};
+let elements = init();
 
-sizeInput.addEventListener('change', saveChanges);
-iconsSizeInput.addEventListener('change', saveChanges);
-centerTextInput.addEventListener('change', saveChanges);
-requiredInput.addEventListener('change', saveChanges);
-centerIsActive.addEventListener('change', saveChanges);
-centerTextColorInput.addEventListener('change', saveChanges);
-itemsColorInput.addEventListener('change', saveChanges);
-itemsLayoutInput.addEventListener('change', saveChanges)
-window.saveChanges = saveChanges;
-customQuestion.onSettingsReceived = setValues;
+function init() {
+    let sizeInput = document.getElementById('size');
+    let iconsSizeInput = document.getElementById('iconsSize');
+    let centerTextInput = document.getElementById('centerText');
+    let requiredInput = document.getElementById('numberRequiredAnswers');
+    let centerIsActive = document.getElementById('centerIsActive');
+    let centerTextColorInput = document.getElementById('centerTextColor');
+    let itemsColorInput = document.getElementById('itemsColor');
+    let itemsLayoutInput = document.getElementById('itemsLayout');
+    let colorNumber = document.getElementById("colorsNumber");
+    let imagesNumber = document.getElementById('imagesNumber');
+    const colors = new Colors(saveChanges);
+    const images = new Images(saveChanges);
 
-function setValues(settings) {
+    sizeInput.addEventListener('input', saveChanges);
+    iconsSizeInput.addEventListener('input', saveChanges);
+    centerTextInput.addEventListener('input', saveChanges);
+    requiredInput.addEventListener('input', saveChanges);
+    centerIsActive.addEventListener('input', saveChanges);
+    centerTextColorInput.addEventListener('input', saveChanges);
+    itemsColorInput.addEventListener('input', saveChanges);
+    itemsLayoutInput.addEventListener('input', saveChanges)
+    window.saveChanges = saveChanges;
+    customQuestion.onSettingsReceived = setValues;
+    return {sizeInput, iconsSizeInput, centerTextInput, requiredInput, centerIsActive, centerTextColorInput, itemsColorInput, itemsLayoutInput, colorNumber, imagesNumber, colors, images};
+}
+
+function setValues(settings, uiSettings, questionSettings) {
     //setInputValue (input, settings);
-    sizeInput.value = settings.sizeSetting;
-    iconsSizeInput.value = settings.iconsSizeSetting;
-    centerTextInput.value = settings.centerTextSetting;
-    requiredInput.value = settings.requiredSetting;
-    centerIsActive.checked = settings.centerIsActiveSetting;
-    centerTextColorInput.value = settings.centerTextColorSetting;
-    itemsColorInput.value = settings.itemsColorSetting;
-    itemsLayoutInput.value = settings.itemsLayoutSetting;
-    colors.init(settings.bullsEyeColorsSetting);
-    images.init(settings.iconsImages)
+    language = uiSettings.currentLanguage;
+    const {sizeInput, iconsSizeInput, centerTextInput, requiredInput, centerIsActive, centerTextColorInput, itemsColorInput, itemsLayoutInput, colorNumber, imagesNumber, colors, images} = elements;
+    sizeInput.value = settings ? settings.sizeSetting : "";
+    iconsSizeInput.value = settings ? settings.iconsSizeSetting: "";
+    centerTextInput.value = settings ? settings.centerTextSetting : "";
+    requiredInput.value = settings ? settings.requiredSetting : "";
+    centerIsActive.checked = settings ? settings.centerIsActiveSetting : false;
+    centerTextColorInput.value = settings ? settings.centerTextColorSetting : "";
+    itemsColorInput.value = settings ? settings.itemsColorSetting : "";
+    itemsLayoutInput.value = settings ? settings.itemsLayoutSetting : "vertical";
+    colors.init(settings ? settings.bullsEyeColorsSetting : 0);
+    images.init(settings ? settings.iconsImages : 0)
 }
 
 function saveChanges() {
+    const {sizeInput, iconsSizeInput, centerTextInput, requiredInput, centerIsActive, centerTextColorInput, itemsColorInput, itemsLayoutInput, colorNumber, imagesNumber, colors, images} = elements;
     let validated = true;
     validated = numberValidate(sizeInput, validated);
     validated = numberValidate(iconsSizeInput, validated);
@@ -48,11 +58,12 @@ function saveChanges() {
     validated = numberValidate (imagesNumber, validated);
     if (!validated)
         return;
-
+    centerTextSetting[language] = centerTextInput.value;
     let settings = {
         sizeSetting: sizeInput.value,
         iconsSizeSetting: iconsSizeInput.value,
         centerTextSetting: centerTextInput.value,
+        //centerTextArraySetting:    centerTextSetting,
         requiredSetting: requiredInput.value,
         centerIsActiveSetting: centerIsActive.checked,
         centerTextColorSetting: centerTextColorInput.value,
